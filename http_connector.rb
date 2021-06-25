298676ece4cff13f5e009f766d94502fd1e29a3c
Khrystyna Klochko <khrystyna.klochko@here.com>
2021-06-25 22:16:09 +0200

    Add main class and http connector

---
+++ http_connector.rb
@@ -0,0 +1,20 @@
+require 'uri'
+require 'net/http'
+
+def fetch_cards
+  uri = URI('https://api.magicthegathering.io/v1/cards')
+  Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+    request = Net::HTTP::Get.new(uri)
+    http.request(request) do |response|
+      if response.is_a?(Net::HTTPSuccess)
+        open('cards.json', 'w') do |file|
+          response.read_body do |chunk|
+            file.write(chunk)
+          end
+        end
+      end
+    end
+  end
+end
+
+
---
+++ main.rb
@@ -0,0 +1,24 @@
+require_relative "http_connector"
+
+def card_commands(option)
+  case option
+  when "-h"
+    puts "Available commands"
+    puts "  -cards-set List of cards grouped by set."
+    puts "  -cards-set-rarity List of cards grouped by set and within each set grouped by rarity."
+    puts "  -cards-ktk List of cards from the Khans of Tarkir (KTK) set that ONLY have the colours red AND blue."
+    exit
+  when "-cards-set"
+    fetch_cards
+  when "-cards-set-rarity"
+    @options[:syntax_highlighting] = true
+  when "-cards-ktk"
+    @options[:syntax_highlighting] = true
+  end
+end
+
+@options = {}
+ARGV.each do |option|
+  puts "Enter -h for help"  unless ["-cards-set", "-cards-set-rarity", "-cards-ktk"].include?(option)
+  card_commands(option) 
+end
